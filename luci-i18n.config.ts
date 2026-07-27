c6333c8916a9de0c5be71ddfecfe05ef0edf4282
LazuliKao <LazuliKao233@outlook.com>
2026-07-27 12:22:08 +0800

    chore: update translate config

--- luci-i18n.config.ts
+++ luci-i18n.config.ts
@@ -1,37 +1,48 @@
 import { defineConfig } from "@lazulikao/luci-types/i18n";
 
 export default defineConfig({
-	packageName: "luci-theme-fluent",
-	input: [
-		"package/luci-theme-fluent/htdocs/luci-static/resources",
-		"src/script/.cache/extra-strings.js",
-	],
-	pot: "package/luci-theme-fluent/po/templates/fluent.pot",
-	extractPot: true,
+  packageName: "luci-theme-fluent",
+  input: ["package/luci-theme-fluent/htdocs/luci-static/resources", "src/script/.cache/extra-strings.js"],
+  pot: "package/luci-theme-fluent/po/templates/fluent.pot",
+  extractPot: true,
 
-	translate: {
-		enabled: true,
-		translator: "openai",
-		batchSize: 20,
-		prompt: "src/script/translate.${locale}.md",
-	},
-
-	locales: [
-		{
-			locale: "zh_Hans",
-			po: "package/luci-theme-fluent/po/zh_Hans/fluent.po",
-		},
-		{
-			locale: "es",
-			po: "package/luci-theme-fluent/po/es/fluent.po",
-		},
-		{
-			locale: "fa",
-			po: "package/luci-theme-fluent/po/fa/fluent.po",
-		},
-		{
-			locale: "ru",
-			po: "package/luci-theme-fluent/po/ru/fluent.po",
-		},
-	],
+  translate: {
+    enabled: true,
+    translator: "openai",
+    batchSize: 10,
+    prompt: "src/script/translate.${locale}.md",
+  },
+  headers: {
+    languageTeam: "LuCI Theme Fluent",
+  },
+  locales: [
+    {
+      locale: "zh_Hans",
+      headers: {
+        lastTranslator: "LazuliKao",
+      },
+      po: "package/luci-theme-fluent/po/zh_Hans/fluent.po",
+    },
+    {
+      locale: "es",
+      headers: {
+        lastTranslator: "castillofrancodamian",
+      },
+      po: "package/luci-theme-fluent/po/es/fluent.po",
+    },
+    {
+      locale: "fa",
+      headers: {
+        lastTranslator: "TranslateGemma",
+      },
+      po: "package/luci-theme-fluent/po/fa/fluent.po",
+    },
+    {
+      locale: "ru",
+      headers: {
+        lastTranslator: "TranslateGemma",
+      },
+      po: "package/luci-theme-fluent/po/ru/fluent.po",
+    },
+  ],
 });
